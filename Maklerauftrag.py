5716a2c7409e2da60bbc873afc66fc4b68681f48
ElenaHolzer-GSO <157456653+ElenaHolzer-GSO@users.noreply.github.com>
2024-02-19 11:02:21 +0100

    Refactored comments

--- Maklerauftrag.py
+++ Maklerauftrag.py
@@ -1,33 +1,33 @@
-# Definiere eine Funktion, um die Fläche eines Raumes zu berechnen.
+# Funktion, um die Flaeche eines Raumes zu berechnen
 def berechne_raumflaeche(laenge, breite):
-    return laenge * breite  # Berechnung der Fläche
+    return laenge * breite  # Berechnung der Flaeche
 
-# Definiere eine leere Liste, um die Informationen der Räume zu speichern.
+# Leere Liste, um die Informationen der Raeume zu speichern.
 raeume = []
 
-# Schleife die läuft, bis zur Eingabe vom Wort 'ende'
+# Schleife die laeuft, bis zur Eingabe von 'ende'
 while True:
     # Eingabe der Raumbezeichnung
     bezeichnung = input("Bitte geben Sie die Raumbezeichnung ein (oder 'ende' zum Beenden): ")
-    if bezeichnung.lower() == 'ende':  # Prüfe, ob der Benutzer das Programm beenden möchte.
+    if bezeichnung.lower() == 'ende':  # Pruefe, ob der Benutzer das Programm beenden moechte
         break
     
-    # Eingabe der Maße des Raumes
+    # Eingabe der Masse des Raumes
     laenge = float(input("Bitte geben Sie die Länge des Raums in Metern ein: "))
     breite = float(input("Bitte geben Sie die Breite des Raums in Metern ein: "))
     
-    # Berechne die Fläche des Raumes und füge sie zur Liste hinzu.
+    # Flaechenberechnung des Raumes und Hinzufuegen zur Liste
     flaeche = berechne_raumflaeche(laenge, breite)
     raeume.append({'Bezeichnung': bezeichnung, 'Fläche': flaeche})
 
-# Initialisiere eine Variable für die Gesamtfläche.
+# Variable fuer Gesamtflaeche initialisieren
 gesamtflaeche = 0
 
-# Gib die Informationen jedes Raumes aus und summiere die Gesamtfläche.
+# Gib die Informationen jedes Raumes aus und summiere die Gesamtflaeche
 print("\nRaumübersicht:")
 for raum in raeume:
     print(f"Raumbezeichnung: {raum['Bezeichnung']}, Fläche: {raum['Fläche']} m²")
     gesamtflaeche += raum['Fläche']  # Addiere die Fläche des Raums zur Gesamtfläche.
 
-# Gib die Gesamtfläche aller Räume aus.
+# Gib die Gesamtflaeche aller Raeume aus.
 print(f"\nGesamtfläche aller Räume: {gesamtflaeche} m²")   
